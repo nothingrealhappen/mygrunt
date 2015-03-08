9f71576a2223c48aa13ba924a0d63161abeb1029
Vanilla <mail@liyaodong.com>
2015-03-08 17:21:56 +0800

    新增index.html以及gitignore

--- mygrunt.sh
+++ mygrunt.sh
@@ -12,7 +12,10 @@ else
     echo '正在初始化项目结构'
     curl -o 'package.json' 'https://raw.githubusercontent.com/liyaodong/mygrunt/master/package.json'
     curl -o  'Gruntfile.js' 'https://raw.githubusercontent.com/liyaodong/mygrunt/master/Gruntfile.js'
+    curl -o  '.gitignore' 'https://raw.githubusercontent.com/liyaodong/mygrunt/master/.gitignore'
+    curl -o  'index.html' 'https://raw.githubusercontent.com/liyaodong/mygrunt/master/index.html'
     setup_project
+    mkdir 'src/css/ src/js/ src/images'
     read -p '项目已初始化完毕是否需要删除本脚本 ？' yn
     case $yn in
         [Yy]* ) rm 'mygrunt.sh'; break;
